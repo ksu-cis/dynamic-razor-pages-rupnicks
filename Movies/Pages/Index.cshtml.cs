8ef91b111f12eb68d1e6431b67a585537585bcf7
Samantha Rupnick <rupnicks@ksu.edu>
2020-04-22 16:26:40 -0500

    Cannot get IMDB min and max to work, will see if implementation helps, though I doubt it

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -12,7 +12,8 @@ namespace Movies.Pages
         /// <summary>
         /// The current search terms
         /// </summary>
-        public string SearchTerms { get; set; }
+        [BindProperty]
+        public string SearchTerms { get; set; } = "";
 
         /// <summary>
         /// The movies to display on the index page
@@ -22,18 +23,37 @@ namespace Movies.Pages
         /// <summary>
         /// The filtered MPAA Ratings
         /// </summary>
+        [BindProperty]
         public string[] MPAARatings { get; set; }
 
+        /// <summary>
+        /// The filtered Genres
+        /// </summary>
+        [BindProperty]
         public string[] Genres { get; set; }
+
+        /// <summary>
+        /// The minimum IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMin { get; set; }
+
+        /// <summary>
+        /// The maximum IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMax { get; set; }
 
         public void OnGet()
         {
-            SearchTerms = Request.Query["SearchTerms"];
             MPAARatings = Request.Query["MPAARatings"];
             Genres = Request.Query["Genres"];
+            IMDBMin = float.Parse(Request.Query["IMDBMin"]);
+            IMDBMax = float.Parse(Request.Query["IMDBMax"]);
             Movies = MovieDatabase.Search(SearchTerms);
             Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
             Movies = MovieDatabase.FilterByGenre(Movies, Genres);
+            //Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
         }
     }
 }
