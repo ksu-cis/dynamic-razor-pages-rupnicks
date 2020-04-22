8cf3446248eb6c8bbec4352b46d83341b8032fdb
Samantha Rupnick <rupnicks@ksu.edu>
2020-04-22 16:37:52 -0500

    Implementing the IMDB Rating Filter

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -126,5 +126,46 @@ namespace Movies
 
             return results;
         }
+
+        /// <summary>
+        /// Filters the collection of movies to those with IMDB ratings within a specified range
+        /// </summary>
+        /// <param name="movies"> The collection of movies to filter </param>
+        /// <param name="min"> The minimum range value </param>
+        /// <param name="max"> The maximum range value </param>
+        /// <returns> The filtered movie collection </returns>
+        public static IEnumerable<Movie> FilterByIMDBRating(IEnumerable<Movie> movies, double? min, double? max)
+        {
+            if (min == null && max == null) return movies;
+
+            var results = new List<Movie>();
+
+            if(min == null)
+            {
+                foreach(Movie movie in movies)
+                {
+                    if (movie.IMDBRating <= max) results.Add(movie);
+                }
+                return results;
+            }
+
+            if(max == null)
+            {
+                foreach(Movie movie in movies)
+                {
+                    if (movie.IMDBRating >= min) results.Add(movie);
+                }
+                return results;
+            }
+
+            foreach(Movie movie in movies)
+            {
+                if(movie.IMDBRating >= min && movie.IMDBRating <= max)
+                {
+                    results.Add(movie);
+                }
+            }
+            return results;
+        }
     }
 }
--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -4,56 +4,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
         /// <summary>
-        /// The current search terms
-        /// </summary>
-        [BindProperty]
-        public string SearchTerms { get; set; } = "";
-
-        /// <summary>
         /// The movies to display on the index page
         /// </summary>
         public IEnumerable<Movie> Movies { get; protected set; }
 
         /// <summary>
-        /// The filtered MPAA Ratings
+        /// Gets and sets the search terms
         /// </summary>
-        [BindProperty]
+        public string SearchTerms { get; set; }
+
+        /// <summary>
+        /// Gets and sets the MPAA rating filters
+        /// </summary>
         public string[] MPAARatings { get; set; }
 
         /// <summary>
         /// The filtered Genres
         /// </summary>
-        [BindProperty]
         public string[] Genres { get; set; }
 
         /// <summary>
-        /// The minimum IMDB Rating
+        /// Gets and sets the IMDB minimium rating
         /// </summary>
-        [BindProperty]
         public double? IMDBMin { get; set; }
 
         /// <summary>
-        /// The maximum IMDB Rating
+        /// Gets and sets the IMDB maximum rating
         /// </summary>
-        [BindProperty]
         public double? IMDBMax { get; set; }
 
-        public void OnGet()
+        /// <summary>
+        /// Does the response initialization for incoming GET requests
+        /// </summary>
+        public void OnGet(double? IMDBMin, double? IMDBMax)
         {
+            SearchTerms = Request.Query["SearchTerms"];
             MPAARatings = Request.Query["MPAARatings"];
             Genres = Request.Query["Genres"];
-            IMDBMin = float.Parse(Request.Query["IMDBMin"]);
-            IMDBMax = float.Parse(Request.Query["IMDBMax"]);
+            // Nullable conversion workaround
+            this.IMDBMin = IMDBMin;
+            this.IMDBMax = IMDBMax;
             Movies = MovieDatabase.Search(SearchTerms);
             Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
             Movies = MovieDatabase.FilterByGenre(Movies, Genres);
-            //Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
+            Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
         }
     }
 }
